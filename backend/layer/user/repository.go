4ad15943de57365dfbfbb9ee3c13ae9374151101
ardhisr1 <radika.kerenz1@gmail.com>
2021-06-11 11:04:08 +0700

    repository user findALl

--- backend/layer/user/repository.go
+++ backend/layer/user/repository.go
@@ -7,8 +7,11 @@ import (
 )
 
 type Repository interface {
-	GetAll() ([]entities.User, error)
+	FindAll() ([]entities.User, error)
 	Create(user entities.User) (entities.User, error)
+	UpdateById(id string, dataUp map[string]interface{}) (entities.User, error)
+	DeleteById(id string) (string, error)
+	FindByID(id string) (entities.User, error)
 }
 
 type repository struct {
@@ -17,4 +20,14 @@ type repository struct {
 
 func NewRepository(db *gorm.DB) *repository {
 	return &repository{db}
+}
+
+func (r *repository) FindAll() ([]entities.User, error) {
+	var users []entities.User
+
+	if err := r.db.Find(&users).Error; err != nil {
+		return users, err
+	}
+
+	return users, nil
 }
